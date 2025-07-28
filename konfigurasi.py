b07bea79b88aa5912f8110f40f1631e804da2b24
Indra Wijaya <indrawijaya180218@gmail.com>
2025-07-28 13:25:28 +0700

    Delete konfigurasi.py

--- konfigurasi.py
+++
@@ -1,9 +0,0 @@
-# konfigurasi.py
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-NAMA_DB = 'konsumsi_obat.db'
-DB_PATH = os.path.join(BASE_DIR, NAMA_DB)
-
-# Waktu minum yang umum
-WAKTU_MINUM = ["Pagi", "Siang", "Sore", "Malam"]
